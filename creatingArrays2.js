73ca6fcd58af01e9aad05138e690c57d01e7d9a6
Alex Joseph <alexandra.k.joseph2323@gmail.com>
2025-03-29 15:01:14 -0500

    Array Fill Updates

---
+++ creatingArrays2.js
@@ -0,0 +1,13 @@
+let greetings = Array(7);
+greetings.fill("Hello");
+console.log(greetings);
+
+greetings.fill("Hi", 1, 4);
+console.log(greetings);
+
+let numArray = Array(5);
+for (let i = 0; i < numArray.length; i++) {
+  numArray[i] = i * 10;
+}
+
+console.log(numArray);
